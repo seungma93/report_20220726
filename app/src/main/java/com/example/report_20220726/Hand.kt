3bfdb4fe4e0203dab73af2da39525d7c927b2976
DESKTOP-B5KNO0J\승민 <seungma93@naver.com>
2022-07-27 17:00:39 +0900

    report_20220727

--- app/src/main/java/com/example/report_20220726/Hand.kt
+++ app/src/main/java/com/example/report_20220726/Hand.kt
@@ -1,9 +1,60 @@
 package com.example.report_20220726
 
-open abstract class Hand (var input : String) {
+abstract open class Hand (){
 
-    var value = input
+    abstract fun fight( value_com : String) : String
 
-    abstract fun fight(a:String)
 
+}
+
+data class Paper (var value : String) : Hand() {
+
+    val paper = value
+
+    override fun fight(value_com: String): String {
+
+        return when (value_com) {
+            "가위" -> "졌다"
+            "바위" -> "이겼다"
+            "보" -> "비겼다"
+            else -> throw IllegalArgumentException("문제발생")
+        }
+    }
+
+
+
+
+}
+
+
+data class Rock(var value: String) : Hand() {
+
+    val rock = value
+
+    override fun fight(value_com: String): String {
+
+        return when (value_com) {
+            "가위" -> "이겼다"
+            "바위" -> "비겼다"
+            "보" -> "졌다"
+            else -> throw IllegalArgumentException("문제발생")
+        }
+
+    }
+}
+
+data class Scissors (var value : String) : Hand() {
+
+    val scissors = value
+
+    override fun fight(value_com: String): String {
+
+        return when (value_com) {
+            "가위" -> "비겼다"
+            "바위" -> "졌다"
+            "보" -> "이겼다"
+            else -> throw IllegalArgumentException("문제발생")
+        }
+
+    }
 }
--- app/src/main/java/com/example/report_20220726/MainActivity.kt
+++ app/src/main/java/com/example/report_20220726/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.report_20220726
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
@@ -10,23 +13,68 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
 
-        val Paper = object : Hand("paper"){
-            override fun fight(a : String) {
+        val text_me = findViewById<TextView>(R.id.text_me)
+        val text_com = findViewById<TextView>(R.id.text_com)
+        val text_result = findViewById<TextView>(R.id.text_result)
+        val button_Paper = findViewById<Button>(R.id.button_Paper)
+        val button_Rock = findViewById<Button>(R.id.button_Rock)
+        val button_Scisseor = findViewById<Button>(R.id.button_Scissors)
 
-                when (a){
+        var paper = Paper("보")
+        var rock = Rock("바위")
+        var scissors = Scissors("가위")
+
+
+                button_Paper.setOnClickListener{
+
+                    var com = Com()
+
+                    var com_choice = com.comResult()
+
+                    var result = paper.fight(com.comResult())
 
 
 
-
-                }
-
-
-
-            }
+                    text_me.text = "나 ${paper.paper}"
+                    text_com.text = "컴 ${com_choice}"
+                    text_result.text = "결과 :${result}"
 
         }
 
+        button_Rock.setOnClickListener{
+
+            var com = Com()
+
+            var com_choice = com.comResult()
+
+            var result = rock.fight(com.comResult())
+
+
+
+            text_me.text = "나 ${rock.rock}"
+            text_com.text = "컴 ${com_choice}"
+            text_result.text = "결과 :${result}"
+
+        }
+
+        button_Scisseor.setOnClickListener{
+
+            var com = Com()
+
+            var com_choice = com.comResult()
+
+            var result = scissors.fight(com.comResult())
+
+
+
+            text_me.text = "나 ${scissors.scissors}"
+            text_com.text = "컴 ${com_choice}"
+            text_result.text = "결과 :${result}"
+
+        }
 
     }
 
+
 }
+
