7a9b818688e2f804d34ce46b1fe25935298c2af3
DESKTOP-B5KNO0J\승민 <seungma93@naver.com>
2022-08-31 18:50:06 +0900

    for windows 20220831

---
+++ app/src/main/java/com/example/report_20220726/ErrorFragment.kt
@@ -0,0 +1,19 @@
+package com.example.report_20220726
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ErrorFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.error, container, false)
+        //frag1과 연결시켜 return해줌.
+    }
+}
