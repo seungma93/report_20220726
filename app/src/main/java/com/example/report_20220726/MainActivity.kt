40291f0c319950c092d0315af554c07b23f273a0
DESKTOP-B5KNO0J\승민 <seungma93@naver.com>
2022-09-04 17:40:53 +0900

    for windows 20220904

--- app/src/main/java/com/example/report_20220726/MainActivity.kt
+++ app/src/main/java/com/example/report_20220726/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.report_20220726
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -15,10 +16,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val textMe = findViewById<TextView>(R.id.text_me)
-        val buttonPaper = findViewById<Button>(R.id.button_Paper)
-        val buttonRock = findViewById<Button>(R.id.button_Rock)
-        val buttonScisseor = findViewById<Button>(R.id.button_Scissors)
+
+        val buttonNext = findViewById<Button>(R.id.button_next)
+
         val editCom = findViewById<EditText>(R.id.com_input)
 
 
@@ -33,11 +33,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        buttonPaper.setOnClickListener {
-            textMe.setText("보")
-            // 본인 플레이어 생성 (보)
-            val me = Player("나", HandValue.Paper)
+        buttonNext.setOnClickListener {
+
+
+
             val transaction = supportFragmentManager.beginTransaction()
+
             val editText = editCom.getText().toString()
 
             if (editText.isEmpty()) {
@@ -47,25 +48,17 @@ class MainActivity : AppCompatActivity() {
                 // 컴퓨터 숫자 받아옴
                 val otherNumber = editText.toInt()
 
-
-                // 컴퓨터 플레이어 생성
-                val playerList = mutableListOf<Player>()
-
-                for (i: Int in 1..otherNumber) {
-                    playerList.add(i - 1, Com("com$i", random()))
+                val intent = Intent(this, PlayerListFragment::class.java)
+                intent.apply{
+                    this.putExtra("playerNum", otherNumber)
                 }
 
-                when (me.playGame(playerList)) {
 
-                    PlayResult.Win -> transaction.replace(R.id.frame_view, WinFragment()).commit()
-                    PlayResult.Lose -> transaction.replace(R.id.frame_view, LoseFragment()).commit()
-                    PlayResult.Draw -> transaction.replace(R.id.frame_view, DrawFragment()).commit()
 
-                }
 
             }
         }
-
+/*
         buttonScisseor.setOnClickListener {
             textMe.setText("가위")
             // 본인 플레이어 생성 (가위)
@@ -131,7 +124,7 @@ class MainActivity : AppCompatActivity() {
 
 
         }
-
+*/
     }
 }
 
